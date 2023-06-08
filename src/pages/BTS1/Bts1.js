17e06b6d318076460d91b8a347d9366ceda0efba
Ryan Williams <ryan.williams93@aol.com>
2023-06-08 16:29:27 -0500

    added page transitions using an AnimatedPage component, temporary solution for logo loading on page using keyframes to adjust opacity

--- src/pages/BTS1/Bts1.js
+++ src/pages/BTS1/Bts1.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import './bts1.css';
+import AnimatedPage from '../../AnimatedPage';
 
 export function Bts1() {
 
+  // Theme Colors
   document.documentElement.style.setProperty('--bg-top', 'rgb(124, 77, 255)');
   document.documentElement.style.setProperty('--bg-bottom', 'rgb(204, 156, 172)');
   document.documentElement.style.setProperty('--outputColor', 'rgb(228, 139, 174)');
@@ -18,7 +19,8 @@ export function Bts1() {
   document.documentElement.style.setProperty('--trigColorInact', 'rgba(255, 255, 255, 0.514)');
   document.documentElement.style.setProperty('--dropMenuBg', 'rgba(90, 76, 139, 0.986)');
   document.documentElement.style.setProperty('--scrollColor', 'rgba(255, 255, 255, 0.432)');
-  
+
+  // State  
   const [expression, setExpression] = useState('');
 
   const ops = ['/', '*', '+', '-', '.'];
@@ -73,100 +75,101 @@ export function Bts1() {
   }
 
   return (
-    <div className="App d-grid">
-      <img className='bg-img' src={require("../../images/Bts-logo-32027.png")} alt="themed-equal-signs"></img>
-      <div 
-        className='
-          calculator
-          mx-auto 
-          my-auto 
-          w-100 
-          p-2 
-          d-grid
-          shadow-lg
-        ' 
-        style={{maxWidth: "370px"}}>
-        <input 
-          type="text" 
-          name="" 
-          id=""
-          placeholder="0" 
+    <AnimatedPage>
+      <div className="App d-grid">
+        <img id="logo" className='bg-img' src={require("../../images/Bts-logo-32027.png")} alt="themed-equal-signs"></img>
+        <div 
           className='
-            form-control
-            form-control-lg
-            bg-dark
-            fs-1 
-            text-end
-            border-0
-            rounded
-            shadow-none
-          '
-          style={{opacity: "1"}} 
-          value={expression}
-          required
-          disabled
-        ></input>
-        <div className='bar'></div>
-        <div className='row row-cols-4 g-2 mt-2 p-3 pt-2'>
-          <div className='col'>
-            <button onClick={clear} className='btn w-100 shadow-lg cac'>C</button>
-          </div>
-          <div className='col-6'>
-            <button onClick={allClear} className='btn w-100 shadow-lg cac'>AC</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>/</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>7</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>8</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg '>9</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>x</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>4</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>5</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>6</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>-</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>1</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>2</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>3</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>+</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg cac'>.</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>0</button>
-          </div>
-          <div className='col-6'>
-            <button onClick={calculate} className='btn w-100 shadow-lg equals'>
-              <img width="50" height="60" src={require("../../images/Bts-logo-32027.png")} alt="themed-equal-signs"></img>
-            </button>
+            calculator
+            mx-auto 
+            my-auto 
+            w-100 
+            p-2 
+            d-grid
+          ' 
+          style={{maxWidth: "370px"}}>
+          <input 
+            type="text" 
+            name="" 
+            id=""
+            placeholder="0" 
+            className='
+              form-control
+              form-control-lg
+              bg-dark
+              fs-1 
+              text-end
+              border-0
+              rounded
+              shadow-none
+            '
+            style={{opacity: "1"}} 
+            value={expression}
+            required
+            disabled
+          ></input>
+          <div className='bar'></div>
+          <div className='row row-cols-4 g-2 mt-2 p-3 pt-2'>
+            <div className='col'>
+              <button onClick={clear} className='btn w-100 shadow-lg cac'>C</button>
+            </div>
+            <div className='col-6'>
+              <button onClick={allClear} className='btn w-100 shadow-lg cac'>AC</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>/</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>7</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>8</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg '>9</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>x</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>4</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>5</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>6</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>-</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>1</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>2</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>3</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>+</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg cac'>.</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>0</button>
+            </div>
+            <div className='col-6'>
+              <button onClick={calculate} className='btn w-100 shadow-lg equals'>
+                <img width="50" height="60" src={require("../../images/Bts-logo-32027.png")} alt="themed-equal-signs"></img>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AnimatedPage>
   );
 }
 
--- src/pages/HOME/Home.js
+++ src/pages/HOME/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import AnimatedPage from '../../AnimatedPage';
 
 export function Home() {
 
@@ -14,7 +15,7 @@ export function Home() {
   document.documentElement.style.setProperty('--cacColor', 'rgba(49, 49, 49, 0.418)');
   document.documentElement.style.setProperty('--cacHoverColor', 'rgb(56, 56, 56)');
   document.documentElement.style.setProperty('--trigColorAct', 'rgb(0, 0, 0)');
-  document.documentElement.style.setProperty('--trigColorInact', 'rgba(145, 124, 6))');
+  document.documentElement.style.setProperty('--trigColorInact', 'rgba(145, 124, 6)');
   document.documentElement.style.setProperty('--dropMenuBg', 'rgba(209, 209, 209, 0.986)');
   document.documentElement.style.setProperty('--scrollColor', 'rgba(5, 70, 16, 0.432)');
 
@@ -73,99 +74,97 @@ export function Home() {
 
 
   return (
-    <div className="App d-grid">
-      {/* <img className='bg-img' alt="themed-equal-signs"></img> */}
-      <div 
-        className='
-          calculator
-          mx-auto 
-          my-auto 
-          w-100 
-          p-2 
-          d-grid
-          shadow-lg
-        ' 
-        style={{maxWidth: "370px"}}>
-        <input 
-          type="text" 
-          name="" 
-          id=""
-          placeholder="0" 
+    <AnimatedPage>
+      <div className="App d-grid">
+        <div 
           className='
-            form-control
-            form-control-lg
-            bg-dark
-            fs-1 
-            text-end
-            border-0
-            rounded
-            shadow-none
-          '
-          style={{opacity: "1"}} 
-          value={expression}
-          required
-          disabled
-        ></input>
-        <div className='bar'></div>
-        <div className='row row-cols-4 g-2 mt-2 p-3 pt-2'>
-          <div className='col'>
-            <button onClick={clear} className='btn w-100 shadow-lg cac'>C</button>
-          </div>
-          <div className='col-6'>
-            <button onClick={allClear} className='btn w-100 shadow-lg cac'>AC</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>/</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>7</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>8</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg '>9</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>x</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>4</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>5</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>6</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>-</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>1</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>2</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>3</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg wa'>+</button>
-          </div>
-          <div className='col'>
-            <button onClick={operator} className='btn w-100 shadow-lg cac'>.</button>
-          </div>
-          <div className='col'>
-            <button onClick={number} className='btn w-100 shadow-lg'>0</button>
-          </div>
-          <div className='col-6'>
-            <button onClick={calculate} className='btn w-100 shadow-lg equals'>=
-              {/* <img width="50" height="60" alt="themed-equal-signs"></img> */}
-            </button>
+            calculator
+            mx-auto 
+            my-auto 
+            w-100 
+            p-2 
+            d-grid
+          ' 
+          style={{maxWidth: "370px"}}>
+          <input 
+            type="text" 
+            name="" 
+            id=""
+            placeholder="0" 
+            className='
+              form-control
+              form-control-lg
+              bg-dark
+              fs-1 
+              text-end
+              border-0
+              rounded
+              shadow-none
+            '
+            style={{opacity: "1"}} 
+            value={expression}
+            required
+            disabled
+          ></input>
+          <div className='bar'></div>
+          <div className='row row-cols-4 g-2 mt-2 p-3 pt-2'>
+            <div className='col'>
+              <button onClick={clear} className='btn w-100 shadow-lg cac'>C</button>
+            </div>
+            <div className='col-6'>
+              <button onClick={allClear} className='btn w-100 shadow-lg cac'>AC</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>/</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>7</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>8</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg '>9</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>x</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>4</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>5</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>6</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>-</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>1</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>2</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>3</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg wa'>+</button>
+            </div>
+            <div className='col'>
+              <button onClick={operator} className='btn w-100 shadow-lg cac'>.</button>
+            </div>
+            <div className='col'>
+              <button onClick={number} className='btn w-100 shadow-lg'>0</button>
+            </div>
+            <div className='col-6'>
+              <button onClick={calculate} className='btn w-100 shadow-lg equals'>=</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AnimatedPage>
   );
 }
